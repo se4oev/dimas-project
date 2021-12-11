2e11341ae7f78d154d270afbc0765e0f4b0149e6
Павел <p.karpenko26@gmail.com>
2021-12-11 18:41:09 +0300

    Поправлено форматирование

--- build/db/patient.sql
+++ build/db/patient.sql
@@ -1,11 +1,12 @@
-CREATE TABLE patient (
-id SERIAL PRIMARY KEY,
-fio VARCHAR(256) NOT NULL,
-birth_date DATE,
-sex NUMERIC(1) NOT NULL,
-num NUMERIC(6),
-smo VARCHAR(256),
-snils VARCHAR(11),
-policy VARCHAR(20),
-fin_source NUMERIC(3)
+CREATE TABLE patient 
+(
+    id         SERIAL PRIMARY KEY,
+    fio        VARCHAR(256) NOT NULL,
+    birth_date DATE,
+    sex        NUMERIC(1) NOT NULL,
+    num        NUMERIC(6),
+    smo        VARCHAR(256),
+    snils      VARCHAR(11),
+    policy     VARCHAR(20),
+    fin_source NUMERIC(3)
 );
