f9a89ae1f13288831ef6b8fcab6936a83612239c
Дмитрий <DmitryPrikhodov>
2021-12-11 17:05:08 +0300

    Добавлен крипт для создания таблицы patient.

---
+++ build/db/patient.sql
@@ -0,0 +1,2 @@
+CREATE TABLE patient (id NUMERIC(12), fio VARCHAR(256), birth_date DATE, sex NUMERIC(1), num NUMERIC(6),
+smo VARCHAR(256), snils VARCHAR(11), policy VARCHAR(20), fin_source NUMERIC(3));
