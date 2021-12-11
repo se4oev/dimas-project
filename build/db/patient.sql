6ab9b03a27e2248938f5a11ca85b737dfc28018d
Дмитрий <DmitryPrikhodov>
2021-12-11 18:19:25 +0300

    Разбил запрос на строки, добавил внешний ключ с автоматической генерацией.

--- build/db/patient.sql
+++ build/db/patient.sql
@@ -1,2 +1,11 @@
-CREATE TABLE patient (id NUMERIC(12), fio VARCHAR(256), birth_date DATE, sex NUMERIC(1), num NUMERIC(6),
-smo VARCHAR(256), snils VARCHAR(11), policy VARCHAR(20), fin_source NUMERIC(3));
+CREATE TABLE patient (
+id SERIAL PRIMARY KEY,
+fio VARCHAR(256) NOT NULL,
+birth_date DATE,
+sex NUMERIC(1) NOT NULL,
+num NUMERIC(6),
+smo VARCHAR(256),
+snils VARCHAR(11),
+policy VARCHAR(20),
+fin_source NUMERIC(3)
+);
