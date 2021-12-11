d68c0f9dc6078db609436dbd4dbbf0ebf296b685
karpenko <karpenko@reshenie-soft.ru>
2021-12-11 15:05:00 +0300

    Add maven to project. Closes #1

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,11 @@
+/**
+ * Created by karpenko on 11.12.2021.
+ * Description:
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
