ce111b1a39eecdab5a399306f22879234800e782
prikhodov <gomunukulus@mail.ru>
2022-02-11 15:39:28 +0300

    Добавил скрипт для создания тестовых данных.

---
+++ build/db/bootstrap.sql
@@ -0,0 +1,131 @@
+INSERT INTO patient
+(fio, birth_date, sex, num, smo, snils, policy, fin_source)
+VALUES
+('Тестовый Тест Тестостеронович',
+'01.09.1939',
+1,
+123456,
+'Застрахуй братуху',
+12345678901,
+1234567890,
+2);
+
+INSERT INTO patient
+(fio, birth_date, sex, num, smo, snils, policy, fin_source)
+VALUES
+('Подвздошный Мафусаил Букибякович',
+'13.12.1973',
+1,
+112365,
+'Застрахуй братуху',
+13856709392,
+09988776655,
+1);
+
+INSERT INTO patient
+(fio, birth_date, sex, num, smo, snils, policy, fin_source)
+VALUES
+('Мимопроходилов Евстрат Хочукушевич',
+'10.02.1965',
+1,
+112749,
+'ИНГОССТРАХ', 16592009392, 09900805655, 2);
+
+INSERT INTO patient
+(fio, birth_date, sex, num, smo, snils, policy, fin_source)
+VALUES
+('Ахеджуева Марта Варфоломеевна',
+'20.07.1935',
+2,
+873749,
+'ИНГОССТРАХ', 11759036392, 09977735655, 2);
+
+INSERT INTO patient
+(fio, birth_date, sex, num, smo, snils, policy, fin_source)
+VALUES
+('Нари Найву Жерве',
+'28.09.1989',
+1,
+878349,
+'Чечен-СТРАХ', 11759055442, 09977674555, 3);
+
+INSERT INTO patient
+(fio, birth_date, sex, num, smo, snils, policy, fin_source)
+VALUES
+('Абу Абдул Азиз ибн Далман',
+'07.07.1979',
+1,
+777349,
+'Альфа Страхование', 58825055442, 09977285065, 1);
+
+INSERT INTO patient
+(fio, birth_date, sex, num, smo, snils, policy, fin_source)
+VALUES
+('Мобу́ту Сесе́ Секо́ Куку Нгбенду ва за Банга',
+'14.10.1930',
+1,
+775129,
+'MetLife', 58995565442, 09977209575, 3);
+
+INSERT INTO patient
+(fio, birth_date, sex, num, smo, snils, policy, fin_source)
+VALUES
+('Салот Сар',
+'19.05.1920',
+1,
+773332,
+'ИНГОССТРАХ', 58442245442, 09951234575, 1);
+
+INSERT INTO patient
+(fio, birth_date, sex, num, smo, snils, policy, fin_source)
+VALUES
+('Краснова Анастасия Анастасовна',
+'19.05.1920',
+2,
+735632,
+'Альфа Страхование', 92820545442, 09959897575, 1);
+
+INSERT INTO patient
+(fio, birth_date, sex, num, smo, snils, policy, fin_source)
+VALUES
+('Мария Склодоская-Кюри',
+'30.11.1950',
+2,
+735991,
+'MetLife', 96144545442, 00917597575, 2);
+
+INSERT INTO patient
+(fio, birth_date, sex, num, smo, snils, policy, fin_source)
+VALUES
+('Ева Дениза Кюри',
+'12.01.1980',
+2,
+741291,
+'MetLife', 90457667442, 00917500565, 2);
+
+INSERT INTO patient
+(fio, birth_date, sex, num, smo, snils, policy, fin_source)
+VALUES
+('Джон Константин',
+'21.06.1979',
+1,
+743434,
+'Застрахуй братуху', 90444667442, 00917575765, 3);
+
+INSERT INTO patient
+(fio, birth_date, sex, num, smo, snils, policy, fin_source)
+VALUES
+('Кукурбито Тыквоголовый',
+'14.09.2001',
+1,
+752634,
+'Застрахуй братуху', 90444690802, 40175575765, 2);
+
+INSERT INTO patient
+(fio, birth_date, sex, num, smo, snils, policy, fin_source)
+VALUES
+('Непомогайло Роман Гивиевич',
+'17.05.1997',
+1,
+762630,
+'Альфа Страхование', 97448643808, 41135576785, 2);
